0a09d4ac95c5be5f321202f8e056e3672f4ce5c1
Kazuki Onishi <princeontrojanhorse@gmail.com>
2023-06-25 09:41:24 +0900

    well

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
+import { Box, Divider, IconButton, Typography } from "@mui/material";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
-import './App.css';
+import "./App.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["PixelMplus"].join(","),
+  },
+});
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Kazuki Onishi</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <IconButton href="https://twitter.com/0n1shi">
+            <TwitterIcon />
+          </IconButton>
+          <IconButton>
+            <GitHubIcon />
+          </IconButton>
+        </Box>
+        <Typography variant="body2">
+          Software developer. I love 📚 🍔 🎮
+        </Typography>
+        <Divider sx={{ width: "60%" }} />
+      </Box>
+    </ThemeProvider>
   );
 }
 
