726d2123153243e00129a545c47206f5c665dd7d
Kazuki Onishi <princeontrojanhorse@gmail.com>
2023-06-26 00:55:53 +0900

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Box,
   IconButton,
@@ -6,55 +6,56 @@ import {
   Table,
   TableRow,
   TableCell,
-  TableCellProps,
-  TableBody,
-} from "@mui/material";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "./App.css";
-import styled from "@emotion/styled";
+  type TableCellProps,
+  TableBody
+} from '@mui/material'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import './App.css'
+import styled from '@emotion/styled'
 
 const theme = createTheme({
   typography: {
-    fontFamily: ["PixelMplus"].join(","),
-  },
-});
+    fontFamily: ['PixelMplus'].join(',')
+  }
+})
 
 const CustomTableCell = styled(TableCell)<TableCellProps>(({ theme }) => ({
-  borderBottom: "none",
-  padding: 0,
-}));
+  borderBottom: 'none',
+  padding: 0
+}))
 
-function App() {
+const links = [
+  { icon: <TwitterIcon />, link: 'https://twitter.com/0n1shi' },
+  { icon: <GitHubIcon />, link: 'https://github.com/0n1shi' }
+]
+
+const affiliations = [
+  { period: '2017/04 ~ 2021/12', name: 'さくらインターネット株式会社' },
+  { period: '2022/01 ~ ', name: 'GMOサイバーセキュリティ byイエラエ株式会社' }
+]
+
+function App (): React.ReactElement {
   return (
     <ThemeProvider theme={theme}>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
         <Typography variant="h4">Kazuki Onishi</Typography>
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
           }}
         >
-          <IconButton href="https://twitter.com/0n1shi" target="_blank">
-            <TwitterIcon />
-          </IconButton>
-          <IconButton href="https://github.com/0n1shi" target="_blank">
-            <GitHubIcon />
-          </IconButton>
-          <IconButton>
-            <InstagramIcon />
-          </IconButton>
+          {links.map(({ icon, link }) => (<IconButton href={link} key={link} target="_blank">{icon}</IconButton>))}
         </Box>
         <Typography variant="body2">
           I am a software developer. I love 💻 📚 🍔 🎮.
@@ -64,33 +65,17 @@ function App() {
         </Typography>
         <Table sx={{ maxWidth: 440, marginTop: 1 }}>
           <TableBody>
-            <TableRow>
-              <CustomTableCell>2017/04 ~ 2021/12</CustomTableCell>
-              <CustomTableCell>: さくらインターネット株式会社</CustomTableCell>
+            {affiliations.map(({ period, name }) => (
+            <TableRow key={name}>
+              <CustomTableCell>{period}</CustomTableCell>
+              <CustomTableCell>: {name}</CustomTableCell>
             </TableRow>
-            <TableRow>
-              <CustomTableCell>2022/01 ~ </CustomTableCell>
-              <CustomTableCell>
-                : GMOサイバーセキュリティ byイエラエ株式会社
-              </CustomTableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Table sx={{ maxWidth: 440, marginTop: 1 }}>
-          <TableBody>
-            <TableRow>
-              <CustomTableCell>: さくらインターネット株式会社</CustomTableCell>
-            </TableRow>
-            <TableRow>
-              <CustomTableCell>
-                : GMOサイバーセキュリティ byイエラエ株式会社
-              </CustomTableCell>
-            </TableRow>
+            ))}
           </TableBody>
         </Table>
       </Box>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
