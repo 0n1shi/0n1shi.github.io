cdc8ddbf73f08ebee0417572a621bb908deb6329
Kazuki Onishi <princeontrojanhorse@gmail.com>
2023-06-25 16:35:24 +0900

    custom divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { Box, Divider, IconButton, Typography } from "@mui/material";
+import React from "react";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Typography,
+  DividerProps,
+} from "@mui/material";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import React from "react";
 import "./App.css";
+import styled from "@emotion/styled";
 
 const theme = createTheme({
   typography: {
     fontFamily: ["PixelMplus"].join(","),
   },
 });
+
+const CustomDivider = styled(Divider)<DividerProps>(({ theme }) => ({
+  width: "60%",
+  marginTop: 10,
+  marginBottom: 10,
+}));
 
 function App() {
   return (
@@ -36,11 +50,20 @@ function App() {
           <IconButton>
             <GitHubIcon />
           </IconButton>
+          <IconButton>
+            <InstagramIcon />
+          </IconButton>
         </Box>
         <Typography variant="body2">
-          Software developer. I love 📚 🍔 🎮
+          Software developer. I love 📚 🍔 🎮.
         </Typography>
-        <Divider sx={{ width: "60%" }} />
+        <CustomDivider />
+        <Typography variant="body2">
+          2017/04 ~ 2021/12: さくらインターネット株式会社
+        </Typography>
+        <Typography variant="body2">
+          2022/01 ~ now: GMOサイバーセキュリティ byイエラエ株式会社
+        </Typography>
       </Box>
     </ThemeProvider>
   );
